628194be3d1f9c22ac2f0d80b8e5c1fc05157e6e
Lucas Mendes <luucas.mds@gmail.com>
2021-02-27 01:47:37 -0300

    Alteracao tempo de countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider( { children }: CountdownProviderPros ) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.06 * 60);
+  const [time, setTime] = useState(0.3 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -34,7 +34,7 @@ export function CountdownProvider( { children }: CountdownProviderPros ) {
 
   const resetCountdown = () => {
     clearTimeout(countdownTimeout);
-    setTime(0.06 * 60);
+    setTime(0.3 * 60);
     setIsActive(false);
     setHasFinished(false);
   }
